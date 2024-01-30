6339899ddac5225ed11040bb702dfe288751d066
Mark Shearing <markshearing@hotmail.com>
2024-01-29 21:02:52 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+# Reference 
+
+timmy = Turtle()
+timmy.fillcolor("aquamarine3")
+timmy.shape("turtle")
+# Move forwards 100
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
