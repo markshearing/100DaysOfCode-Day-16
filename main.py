16116b298f2caf05a5a8395c50555c20297cb984
Mark Shearing <33139782+markshearing@users.noreply.github.com>
2024-01-29 21:57:19 -0500

    No real change

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ table = PrettyTable()
 table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
 table.add_column("Type",["Electric","Water","Fire"])
 table.align = "l"
-table.
+
 print(table)
