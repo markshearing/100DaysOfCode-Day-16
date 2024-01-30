3a2e23cbf2b71f00cda0bbac2a69f5fa414bd9b5
Mark Shearing <markshearing@hotmail.com>
2024-01-30 02:49:53 +0000

    Switched over to use PrettyTable

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-# Reference 
+# # Reference 
 
-timmy = Turtle()
-timmy.fillcolor("aquamarine3")
-timmy.shape("turtle")
-# Move forwards 100
-timmy.forward(100)
+# timmy = Turtle()
+# timmy.fillcolor("aquamarine3")
+# timmy.shape("turtle")
+# # Move forwards 100
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
+table.
+print(table)
